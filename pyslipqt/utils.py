f6700c5ccd9961cf30ac9ee3a862bb7c555f3533
Ross Wilson <rzzzwilson@gmail.com>
2018-02-20 16:29:09 +0700

    New module for utility functions

---
+++ pyslipqt/utils.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""
+Small utility functions.
+"""
+
+
+import traceback
+# if we don't have log.py, don't crash
+try:
+#    from . import log
+    import log
+    log = log.Log('pyslipqt.log')
+except AttributeError:
+    # means log already set up
+    pass
+except ImportError as e:
+    # if we don't have log.py, don't crash
+    # fake all log(), log.debug(), ... calls
+    def logit(*args, **kwargs):
+        pass
+    log = logit
+    log.debug = logit
+    log.info = logit
+    log.warn = logit
+    log.error = logit
+    log.critical = logit
+
+
+def str_trace(msg=None):
+    """Get a traceback string.
+
+    This is useful if we need at any point in code to find out how
+    we got to that point.
+    """
+
+    result = []
+
+    if msg:
+        result.append(msg+'\n')
+
+    result.extend(traceback.format_stack())
+
+    return ''.join(result)
+
+
+def log_trace(msg=None):
+    """Log a traceback string."""
+
+    log.debug(str_trace(msg))
