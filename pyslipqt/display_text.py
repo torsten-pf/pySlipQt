8f465f8ff3f0b3b39369a042b156c110d1c003ce
Ross Wilson <rzzzwilson@gmail.com>
2018-09-14 18:48:04 +0700

    Better widgets

--- pyslipqt/display_text.py
+++ pyslipqt/display_text.py
@@ -25,116 +25,57 @@ Methods:
 
 """
 
-from PyQt5 import QtCore
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
-from PyQt5.QtWidgets import QLabel, QSpinBox, QGroupBox, QCheckBox, QSpacerItem
-from PyQt5.QtCore import pyqtSignal
-
+from PyQt5.QtWidgets import *
+import sys
 
 class DisplayText(QWidget):
 
-    # size of the widget
-    WidgetWidth = 80
-    WidgetHeight = 30
-    LabelXOffset = 5
-    LabelYOffset = 5
-    LabelHeight = 24
-    LabelWidth = 40
-    TextXOffset = 40
-    TextYOffset = LabelYOffset
-    TextHeight = LabelHeight
-    TextWidth = 35
+#    # set platform dependant values
+#    if platform.system() == 'Linux':
+#        pass
+#    elif platform.system() == 'Darwin':
+#        pass
+#    elif platform.system() == 'Windows':
+#        pass
+#    else:
+#        raise Exception('Unrecognized platform: %s' % platform.system())
 
-    def __init__(self, title='', label='', text='', tooltip=None, width=None):
+    text_style = ('background-color: rgb(240, 240, 240);'
+                  'border:1px solid rgb(128, 128, 128);'
+                  'border-radius: 3px;'
+                 )
+
+    def __init__(self, title, label, tooltip=None):
         QWidget.__init__(self)
-        self.setContentsMargins(0,0,0,0)
-#        self.setStyleSheet("font-style: normal;font-size: 10pt;font-weight: bold;");
-        self.setStyleSheet("font-style: normal; font-size: 10pt;background-color: rgb(232, 255, 255);");
 
-        # set the style for some sub-widgets
-        label_style = 'margin-top: 10px; '
+        # create all widgets
+        lbl_label = QLabel(label)
+        self.lbl_text = QLabel()
+        self.lbl_text.setStyleSheet(self.text_style)
 
-        text_style = ('background-color: rgb(232, 232, 232);'
-                      'border:1px solid rgb(128, 128, 128);'
-                      'border-radius: 3px;'
-                      'margin-top: 10px;')
-
-        groupbox_style = ('QGroupBox {'
-                          '    border: 1px solid rgb(128, 128, 128);'
-                          '    border-radius: 3px;'
-                          '    background-color: rgb(232, 255, 255);'
-                          '}'
-                          'QGroupBox:title {'
-                          '    subcontrol-origin: margin;'
-                          '    subcontrol-position: top left;'
-                          '    padding-left: 0px;'
-                          '    padding-right: 0px;'
-                          '    padding-top: 0px;'
-                          '    padding-bottom: 0px;'
-                          '    font-weight: bold;'
-#                          '    border-top: none;'
-                          '}'
-                         )
-
-        # define the widgets we are going to use
-        lbl_label = QLabel(label, self)
-        lbl_label.setFixedHeight(self.LabelHeight)
-        lbl_label.setFixedWidth(self.LabelWidth)
-        lbl_label.setAlignment(Qt.AlignRight|Qt.AlignCenter)
-        lbl_label.setStyleSheet(label_style)
-        lbl_label.setGeometry(QtCore.QRect(self.LabelXOffset, self.LabelYOffset,
-                                           self.LabelWidth, self.LabelHeight)) #(x, y, width, height)
-
-        self.lbl_text = QLabel(text, self)
-#        self.lbl_text.setFixedHeight(self.LabelHeight)
-#        if width:
-#            self.lbl_text.setFixedWidth(width)
-#        else:
-#            self.lbl_text.setFixedWidth(self.TextWidth)
-        self.lbl_text.setGeometry(QtCore.QRect(self.TextXOffset, self.TextYOffset,
-                                               self.TextWidth, self.TextHeight)) #(x, y, width, height)
-        self.lbl_text.setAlignment(Qt.AlignLeft) # |Qt.AlignCenter)
-        self.lbl_text.setStyleSheet(text_style)
-    
-        # start the layout
-        layout = QVBoxLayout()
-        print(dir(layout))
-        layout.setContentsMargins(0,0,0,0)
+        # start layout
+        layout = QGridLayout()
 
         groupbox = QGroupBox(title)
-        groupbox.setStyleSheet(groupbox_style)
-        groupbox.setContentsMargins(0,0,0,0)
-
         layout.addWidget(groupbox)
 
-        vbox = QVBoxLayout()
-
         hbox = QHBoxLayout()
+        groupbox.setLayout(hbox)
+
         hbox.addWidget(lbl_label)
         hbox.addWidget(self.lbl_text)
         hbox.addStretch()
 
-        vbox.addLayout(hbox)
-        vbox.addStretch()
-
-        groupbox.setLayout(vbox)
-
         self.setLayout(layout)
 
-        # if tooltip
         if tooltip:
             self.setToolTip(tooltip)
 
-        # set height of the widget
-        self.setFixedHeight(self.WidgetHeight + 20)
-        self.show()
 
     def set_text(self, text):
-        """Set the text in the widget.
+        """Set the text of the display field.
 
-        text  the string to put intoi the text field
+        text the text to show
         """
 
         self.lbl_text.setText(text)
-        self.update()
--- pyslipqt/layer_control.py
+++ pyslipqt/layer_control.py
@@ -28,10 +28,20 @@ Events:
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt, pyqtSignal
-from PyQt5.QtWidgets import QWidget, QCheckBox, QGroupBox, QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy
-
+from PyQt5.QtWidgets import (QWidget, QCheckBox, QGroupBox, QVBoxLayout,
+                             QHBoxLayout, QSpacerItem, QSizePolicy, QGridLayout)
 
 class LayerControl(QWidget):
+
+#    # set platform dependant values
+#    if platform.system() == 'Linux':
+#        pass
+#    elif platform.system() == 'Darwin':
+#        pass
+#    elif platform.system() == 'Windows':
+#        pass
+#    else:
+#        raise Exception('Unrecognized platform: %s' % platform.system())
 
     # signals raised by this widget
     change_add = pyqtSignal(bool)       # signal raised when user toggles "add" checkbox
@@ -39,19 +49,8 @@ class LayerControl(QWidget):
     change_select = pyqtSignal(bool)    # signal raised when user toggles "select" checkbox
 
     # size of the widget
-    WidgetWidth = 80
-    AddXOffset = 0
-    AddYOffset = 5
-    AddWidth = 10
-    AddHeight = 10
-    ShowXOffset = 35
-    ShowYOffset = AddYOffset
-    ShowWidth = 10
-    ShowHeight = 10
-    SelectXOffset = 60
-    SelectYOffset = AddYOffset
-    SelectWidth = 10
-    SelectHeight = 10
+    WidgetWidth = 160
+    WidgetHeight = 80
 
 
     def __init__(self, parent, title, selectable=False, tooltip=None):
@@ -64,136 +63,67 @@ class LayerControl(QWidget):
         """
 
         QWidget.__init__(self)
-        self.setContentsMargins(0,0,0,0)
-        self.setStyleSheet("font-style: normal; font-size: 10pt;background-color: rgb(232, 255, 255);");
 
-        self.selectable = selectable
+        # create all widgets
+        self.cb_show = QCheckBox('Show')
+        self.cb_show.setChecked(True)
+        self.cb_select = QCheckBox('Select')
 
-        groupbox_style = ('QGroupBox {'
-                          '    border: 1px solid rgb(128, 128, 128); '
-                          '    border-radius: 3px;'
-                          '    background-color: rgb(232, 255, 255); '
-                          '}'
-                          'QGroupBox:title {'
-                          '    subcontrol-origin: margin;'
-                          '    subcontrol-position: top left;'
-                          '    padding-left: 0px;'
-                          '    padding-right: 0px;'
-                          '    padding-top: 0px;'
-                          '    padding-bottom: 0px;'
-                          '    font-weight: bold;'
-#                          '    border-top: none;'
-                          '}'
-                         )
-
-        # define the widgets we are going to use
-        self.cb_add = QCheckBox('Add layer', self)
-        self.cb_add.setGeometry(QtCore.QRect(self.AddXOffset, self.AddYOffset,
-                                             self.AddWidth, self.AddHeight)) #(x, y, width, height)
-
-        self.cb_show = QCheckBox('Show', self)
-        self.cb_show.setGeometry(QtCore.QRect(self.ShowXOffset, self.ShowYOffset,
-                                              self.ShowWidth, self.ShowHeight)) #(x, y, width, height)
-        self.cb_show.setEnabled(False)
-
-        if selectable:
-            self.cb_select = QCheckBox('Select', self)
-            self.cb_select.setGeometry(QtCore.QRect(self.SelectXOffset, self.SelectYOffset,
-                                                    self.SelectWidth, self.SelectHeight)) #(x, y, width, height)
-            self.cb_select.setEnabled(False)
-
-        # start the layout
-        layout = QHBoxLayout()
+        # start layout
+        layout = QGridLayout()
 
         groupbox = QGroupBox(title)
-        groupbox.setStyleSheet(groupbox_style)
-
-        layout.addWidget(groupbox)
+        groupbox.setCheckable(True)
+        groupbox.setChecked(False)
+        layout.addWidget(groupbox, 0, 0)
 
         hbox = QHBoxLayout()
-        hbox.addWidget(self.cb_add)
-        hbox.addWidget(self.cb_show)
-        if selectable:
-            hbox.addWidget(self.cb_select)
-#        hbox.addLayout(hbox)
-
         groupbox.setLayout(hbox)
 
-        self.setLayout(layout)
+        hbox.addWidget(self.cb_show)
+        hbox.addWidget(self.cb_select)
+        hbox.addStretch()
 
-######
-#        # start the layout
-#        layout = QVBoxLayout()
-#
-#        groupbox = QGroupBox(title)
-#        groupbox.setStyleSheet(groupbox_style)
-#
-#        hbox = QHBoxLayout()
-##        hbox.addStretch()
-#        hbox.addWidget(self.cb_show)
-#        if selectable:
-#            hbox.addWidget(self.cb_select)
-#
-#        vbox = QVBoxLayout()
-#        vbox.addWidget(self.cb_add)
-#        vbox.addLayout(hbox)
-#
-##        layout.addWidget(groupbox)
-#        groupbox.setLayout(vbox)
-#
-#        self.setLayout(layout)
+        self.setLayout(layout)
 
         # if tooltip given, set it up
         if tooltip:
             self.setToolTip(tooltip)
 
         # connect internal widget events to handlers
-        self.cb_add.stateChanged.connect(self.changed_add)
+        groupbox.toggled.connect(self.changed_add)
         self.cb_show.stateChanged.connect(self.changed_show)
         if selectable:
             self.cb_select.stateChanged.connect(self.changed_select)
 
         self.show()
 
-    def createExampleGroup(self):
-        groupBox = QGroupBox("Best Food")
+    def changed_add(self, state):
+        """Main checkbox changed.
+       
+        state  the state of the groupbox check: True == ticked
 
-        radio1 = QRadioButton("&Radio pizza")
-        radio2 = QRadioButton("R&adio taco")
-        radio3 = QRadioButton("Ra&dio burrito")
+        Emit a signal with the state.
+        """
 
-        radio1.setChecked(True)
+        self.change_add.emit(state)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
-        vbox.addWidget(radio3)
-        vbox.addStretch(1)
-        groupBox.setLayout(vbox)
+    def changed_show(self, state):
+        """'Show' checkbox changed.
+       
+        state  the state of the 'Show' check: True == ticked
 
-        return groupBox
+        Emit a signal with the state.
+        """
 
-    def changed_add(self):
-        """Main checkbox changed."""
+        self.change_show.emit(state)
 
-        if self.cb_add.checkState():
-            # if "add" is ON
-            self.cb_show.setEnabled(True)       # enable and set "show"
-            if self.selectable:
-                self.cb_select.setEnabled(True) # enable "select", if shown
-        else:
-            self.cb_show.setEnabled(False)
-            if self.selectable:
-                self.cb_select.setEnabled(False)
+    def changed_select(self, state):
+        """'Select' checkbox changed.
+       
+        state  the state of the 'Select' check: True == ticked
 
-        self.change_add.emit(self.cb_add.checkState())
+        Emit a signal with the state.
+        """
 
-    def changed_show(self):
-        """Show checkbox changed."""
-
-        self.change_show.emit(self.cb_show.checkState())
-
-    def changed_select(self):
-        """Select checkbox changed."""
-
-        self.change_select.emit(self.cb_select.checkState())
+        self.change_select.emit(state)
