daeedac7085e829459ada6e56da58c8b909c8c6d
Ross Wilson <rzzzwilson@gmail.com>
2018-10-21 14:40:39 +0700

    Include the 'examples' code

--- setup.py
+++ setup.py
@@ -71,32 +71,3 @@ setup(name='pySlipQt',
       download_url='https://github.com/rzzzwilson/pySlipQt/releases/tag/0.1.0',
       include_package_data=True,
       zip_safe=False)
-
-
-
-#from setuptools import setup, find_packages
-#
-#def readme():
-#    with open('README.rst') as f:
-#        return f.read()
-#
-#setup(name='pySlipQt',
-#      version='0.1.0',
-#      description='A slipmap widget for PyQt5',
-#      long_description=readme(),
-#      url='http://github.com/rzzzwilson/pySlipQt',
-#      author='Ross Wilson',
-#      author_email='rzzzwilson@gmail.com',
-#      license='MIT',
-#      packages=find_packages(),
-#                  ],
-#      install_requires=['pyqt5', 'python'],
-#      classifiers=['Development Status :: 3 - Alpha',
-#                   'Intended Audience :: Developers',
-#                   'License :: OSI Approved :: MIT License',
-#                   'Operating System :: OS Independent',
-#                   'Programming Language :: Python :: 3 :: Only'],
-#      keywords='python pyqt5 slipmap map',
-#      download_url='https://github.com/rzzzwilson/pySlipQt/releases/tag/0.1.0',
-#      include_package_data=True,
-#      zip_safe=False)
