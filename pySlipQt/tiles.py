360f7bb21fbf21b08fb384db85685b4a89f7e425
Ross Wilson <rzzzwilson@gmail.com>
2019-05-19 11:45:36 +0700

    Added log message if tiles directory doesn't exist

--- pySlipQt/tiles.py
+++ pySlipQt/tiles.py
@@ -154,8 +154,9 @@ class BaseTiles(object):
                 msg = ("%s doesn't appear to be a tile cache directory"
                        % tiles_dir)
                 raise Exception(msg) from None
-            log.critical("The tiles directory %s doesn't exist, creating it."
-                         % tiles_dir)
+            msg = "The tiles directory %s doesn't exist, creating it." % tiles_dir
+            log.critical(msg)
+            print(msg)
             os.makedirs(tiles_dir)
 
 # possible recursion here?
